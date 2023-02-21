7d042908a81943c310100eaa288087907abaada2
Gabreil <bieel_oliveira3427@hotmail.com>
2023-02-21 20:33:18 -0300

    Initial Commit

---
+++ ConexiaLogin/ConexiaLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConexiaLoginApp.swift
+//  ConexiaLogin
+//
+//  Created by Gabriel Oliveira on 21/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ConexiaLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
