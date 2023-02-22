5962bba436cced90c72398750f485cb6b482c416
Gabreil <bieel_oliveira3427@hotmail.com>
2023-02-21 22:13:45 -0300

    Conexia Login é um projeto de interface de usuário (front-end) desenvolvido em Swift para iOS. O projeto foi criado como um teste e é o primeiro projeto desenvolvido por mim.
    O projeto é voltado para a experiência de login do usuário e oferece uma tela de login personalizável e fácil de usar para o sistema operacional iOS. Embora o projeto seja apenas para o front-end em Swift, ele pode ser facilmente integrado a diferentes tecnologias back-end para fornecer uma solução completa de autenticação para aplicações iOS.
    
    Como um projeto inicial, Conexia Login pode ser considerado como uma prova de conceito para futuros projetos mais complexos. O código-fonte do projeto está disponível no repositório do GitHub para que outros desenvolvedores possam estudá-lo, contribuir com melhorias e aprendam a partir dele.

--- ConexiaLogin/ContentView.swift
+++ ConexiaLogin/ContentView.swift
@@ -8,19 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color("BackgroundColor").ignoresSafeArea()
+            
+            
+            
+            VStack{
+                
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                VStack{
+                    
+                    TextField("Email", text: $email)
+                        .padding(.top, 12)
+                        .padding(.bottom, 24)
+                        .padding(.horizontal)
+                    Divider()
+                    TextField("Senha", text: $password)
+                        .padding(.top, 24)
+                        .padding(.bottom, 12)
+                        .padding(.horizontal)
+                }.background(Color.white)
+                    .cornerRadius(4.0)
+                
+                
+                Button(action: {}){
+                    
+                    Text("Login")
+                        .font(.title3)
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(20)
+                        .frame(maxWidth: .infinity)
+                        .background(Color("ButtonColor"))
+                        .cornerRadius(4.0)
+                    
+                }.padding(100)
+                Button(action: {}){
+                    
+                    Text("Não tem uma conta? Crie a sua agora.")
+                        .foregroundColor(.white)
+                        .bold()
+                        
+            }
+                Button(action: {}){
+                    
+                    Text("Esqueceu sua senha?")
+                        .foregroundColor(.white)
+                        .bold()
+            }
+                
+            }
+        
+            
         }
-        .padding()
+            
+        }
+        
+        
+        
+        
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
+        
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
